e8df6d4c9f1c491d49a1fbaac3d8bbb9e286832a
Anton Volkov <anton.volkov@zillion.games>
2024-10-28 12:29:24 -0100

    feat(batch): test for failed batch csv save

--- src/main/java/com/altinntech/clicksave/core/BatchCollector.java
+++ src/main/java/com/altinntech/clicksave/core/BatchCollector.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
