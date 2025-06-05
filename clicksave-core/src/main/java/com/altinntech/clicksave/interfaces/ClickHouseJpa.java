f40fa2384834548c4711c586a743b4bc12684306
Anton Volkov <anton.wolches@gmail.com>
2025-06-05 14:40:58 +0300

    feat(refactoring): Backward compatibility

--- clicksave-core/src/main/java/com/altinntech/clicksave/interfaces/ClickHouseJpa.java
+++ clicksave-core/src/main/java/com/altinntech/clicksave/interfaces/ClickHouseJpa.java
@@ -3,6 +3,7 @@ package com.altinntech.clicksave.interfaces;
 import com.altinntech.clicksave.annotations.SettableQuery;
 import lombok.SneakyThrows;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
@@ -79,4 +80,24 @@ public interface ClickHouseJpa<T, ID> extends ClicksaveUtils {
 
     @SneakyThrows
     <R> Optional<R> findSingleCustomQuery(Class<R> producer, String query, List params);
+
+    /**
+     * Deprecated in favor of using java.util.List
+     * Depracated because of UB when passing Object[] to varargs method (Object[N] can be treated as Object[1][N])
+     */
+    @Deprecated(since = "1.2.1")
+    @SneakyThrows
+    default <R> List<R> findAllCustomQuery(Class<R> producer, String query, Object ... params) {
+        return findAllCustomQuery(producer, query, Arrays.asList(params));
+    }
+
+    /**
+     * Deprecated in favor of using java.util.List
+     * Deprecated because of UB when passing Object[] to varargs method (Object[N] can be treated as Object[1][N])
+     */
+    @Deprecated(since = "1.2.1")
+    @SneakyThrows
+    default <R> Optional<R> findSingleCustomQuery(Class<R> producer, String query, Object ... params) {
+        return findSingleCustomQuery(producer, query, Arrays.asList(params));
+    }
 }
